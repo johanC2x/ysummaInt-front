6f9089437241b07276cd99520ee0392a7c6ad433
johanc93 <johan.canari@tcs.com>
2021-08-04 23:41:14 -0500

    FIX

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.155.92.156:8080/YsummaInt/',
+  apiUrl: 'http://192.155.92.156:8080/YsummaInt',
   apiDomain: '192.155.92.156:8080'
 };
